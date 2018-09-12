de68147d914def07a0c9fcf7606728fa194ef5c3
Evan Benn <evanbenn@gmail.com>
2018-09-12 17:39:31 +1000

    Travis upload toccer

--- .travis/push.sh
+++ .travis/push.sh
@@ -12,7 +12,7 @@ commit_website_files() {
 
 upload_files() {
   git remote add origin-pages https://${GH_TOKEN}@github.com/EvanTheB/wdl.git > /dev/null 2>&1
-  git push --quiet --set-upstream origin-pages HEAD:TRAVIS_BRANCH
+  git push --quiet --set-upstream origin-pages HEAD:$TRAVIS_BRANCH
 }
 
 setup_git
